049a7cc30f4cc7c1508596257a566c467c8a08e0
Jenny Tang <tangjenny1224@gmail.com>
2022-03-14 21:44:59 -0700

    debug

--- src/main/java/RunBroker.java
+++ src/main/java/RunBroker.java
@@ -4,9 +4,15 @@ import java.util.List;
 public class RunBroker {
     public static void main(String[] args){
         List<Object> maps = Utilities.readConfig();
+        System.out.println(maps.size());
         IPMap ipMap = (IPMap) maps.get(0);
+        System.out.println(ipMap.toString());
         PortMap portMap = (PortMap) maps.get(1);
+        System.out.println(portMap.toString());
         String brokerHostName = Utilities.getHostName();
+        System.out.println(brokerHostName);
+        System.out.println(ipMap.getIdByIP(brokerHostName));
+        System.out.println(portMap.getPortById(ipMap.getIdByIP(brokerHostName)));
         int brokerPort = Integer.parseInt(portMap.getPortById(ipMap.getIdByIP(brokerHostName)));
         Broker broker = new Broker(brokerHostName, brokerPort);
         try {
