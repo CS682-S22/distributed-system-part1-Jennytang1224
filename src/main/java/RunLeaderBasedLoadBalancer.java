00e19f7cd781fbeddbea70b580a2f47bc522ff4a
Jenny Tang <tangjenny1224@gmail.com>
2022-04-13 14:29:22 -0700

    changed the protobuf, send change instead of the whole table to LB

---
+++ src/main/java/RunLeaderBasedLoadBalancer.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * run load balancer
+ */
+public class RunLeaderBasedLoadBalancer {
+    public static void main(String[] args){
+//        // 3 5 (3 broker and 5 partitions) brokerConfig
+//        if(!Utilities.validateArgsLoadBalancer(args)){
+//            System.exit(-1);
+//        }
+//
+//        int numOfBrokers = Integer.parseInt(args[0]);
+//        int numOfPartitions = Integer.parseInt(args[1]);
+//        String brokerConfigFile = args[2];
+
+//        List<Object> maps = Utilities.readConfig();
+//        IPMap ipMap = (IPMap) maps.get(0);
+//        PortMap portMap = (PortMap) maps.get(1);
+//        String LBHostName = Utilities.getHostName();
+
+        String LBHostName = "Jennys-MacBook-Pro.local";
+        int LBPort = 1430;
+        LeaderBasedLoadBalancer loadBalancer = new LeaderBasedLoadBalancer(LBHostName, LBPort);
+
+        try {
+            loadBalancer.run();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
