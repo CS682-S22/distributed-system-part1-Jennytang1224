711fac297fc5128b5cfa910bc232e1c7ae10af40
Jenny Tang <tangjenny1224@gmail.com>
2022-04-21 12:48:01 -0700

    test

--- src/main/java/HeartBeatListener.java
+++ src/main/java/HeartBeatListener.java
@@ -11,7 +11,7 @@ public class HeartBeatListener implements Runnable {
     MembershipTable membershipTable;
     CS601BlockingQueue<Resp.Response> bq;
     private ExecutorService executor;
-    int delay = 450;
+    int delay = 500;
     int retires = 3;
     int peerID;
     volatile boolean sending;
